7b454aec8aa9945a9676590d500a9724d0114294
Zach Leffke <zleffke@gmail.com>
2023-02-25 12:05:18 -0500

    initial version appears to be working!

--- process_sonde.py
+++ process_sonde.py
@@ -14,10 +14,12 @@ import math
 import copy
 from binascii import *
 import pandas as pd
+import numpy as np
 
 from utilities import RadioSonde as rs
 from utilities import plotting
 from utilities import astro
+from utilities import Kalman
 
 obs = {}
 cnt = 0
@@ -123,26 +125,76 @@ if __name__ == '__main__':
     deg2rad = math.pi / 180.0
     # result = razel.LLH_To_ECEF(df['lat'] * deg2rad, df['lon']*deg2rad, df['alt']/1000.0)
 
-    df_ecef = pd.DataFrame(df_meas.apply(lambda x: astro.LLH_To_ECEF(x['lat']*deg2rad, 
-                                                                     x['lon']*deg2rad, 
-                                                                     x['alt']/1000.0), axis=1).to_list(), 
-                                                                     columns=['i','j','k'])
+    df_pos = pd.DataFrame(df_meas.apply(lambda x: astro.LLH_To_ECEF(x['lat']*deg2rad, 
+                                                                    x['lon']*deg2rad, 
+                                                                    x['alt']/1000.0), axis=1).to_list(), 
+                                                                    columns=['i','j','k'])
+    df_pos['i'] = df_pos['i'].apply(lambda x: x*1000.0)
+    df_pos['j'] = df_pos['j'].apply(lambda x: x*1000.0)
+    df_pos['k'] = df_pos['k'].apply(lambda x: x*1000.0)
   
-    df_dot = pd.DataFrame(df_meas.apply(lambda x: astro.SpeedHeading_To_ECEF(x['lat']*deg2rad, 
+    df_vel = pd.DataFrame(df_meas.apply(lambda x: astro.SpeedHeading_To_ECEF(x['lat']*deg2rad, 
                                                                     x['lon']*deg2rad, 
                                                                     x['vel_h'],
                                                                     x['vel_v'],
                                                                     x['heading']*deg2rad,
                                                                     km=True), axis=1).to_list(), 
                                                                     columns=['i_dot','j_dot','k_dot'])
+    df_vel['i_dot'] = df_vel['i_dot'].apply(lambda x: x*1000.0)
+    df_vel['j_dot'] = df_vel['j_dot'].apply(lambda x: x*1000.0)
+    df_vel['k_dot'] = df_vel['k_dot'].apply(lambda x: x*1000.0)
 
-    df_meas = pd.concat([df_meas, df_ecef, df_dot], axis=1, join='inner')
+    df_meas = pd.concat([df_meas, df_pos, df_vel], axis=1, join='inner')
+
     df_meas.name = cfg['serial']
 
     print(df_meas)
     print(df_meas.name)
-    # df_meas['i'] a[0]
-    # df_measa['i']['j'], df_meas['k'])
+
+
+    
+
+    print(cfg['kalman'])
+    kf_cfg = cfg['kalman']
+    kf = Kalman.KalmanFilter_3D(dt  = kf_cfg['sample_time'],
+                                u_a = kf_cfg['u_a'],
+                                std_acc = kf_cfg['std_acc'],
+                                pos_std_meas = kf_cfg['pos_std_meas'],
+                                vel_std_meas = kf_cfg['vel_std_meas'])
+    
+    initial_state = (df_meas['i'][0],
+                     df_meas['j'][0],
+                     df_meas['k'][0],
+                     df_meas['i_dot'][0],
+                     df_meas['j_dot'][0],
+                     df_meas['k_dot'][0]) 
+    print(initial_state)
+    kf.set_initial_state(initial_state)
+
+    kf.print_parameters()
+    
+    
+    
+    for i in range(len(df_meas[1:-1])):
+        k  = kf.predict()
+        print("   k:", k.tolist())
+
+        meas = np.matrix([[df_meas['i'][i+1]],
+                          [df_meas['j'][i+1]],
+                          [df_meas['k'][i+1]],
+                          [df_meas['i_dot'][i+1]],
+                          [df_meas['j_dot'][i+1]],
+                          [df_meas['k_dot'][i+1]]])
+        print("meas:", meas.tolist())
+
+
+        k1 = kf.update(meas)
+        print("  k1:", k1.tolist())
+        print()
+
+        time.sleep(1)
+
+
     
 
 
